791d2470cb5fdba1ff2e5c6258f9fbec22fe0cf8
Fábio Santos <fabiosantos.pe@gmail.com>
2018-11-20 16:16:41 -0300

    Updates

---
+++ precache-manifest.6c8d989d453b55b28d42b9e5d7f4b828.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d40730c9200d7ba8e42",
+    "url": "/teste-jogo-da-velha-pwa/static/js/runtime~main.7d40730c.js"
+  },
+  {
+    "revision": "c25ce4b591a0f57511d9",
+    "url": "/teste-jogo-da-velha-pwa/static/js/main.c25ce4b5.chunk.js"
+  },
+  {
+    "revision": "09a879202fa24e7f10e7",
+    "url": "/teste-jogo-da-velha-pwa/static/js/1.09a87920.chunk.js"
+  },
+  {
+    "revision": "c25ce4b591a0f57511d9",
+    "url": "/teste-jogo-da-velha-pwa/static/css/main.a0b5cc88.chunk.css"
+  },
+  {
+    "revision": "6cdc9a7fc70345e501fc1508438bfda3",
+    "url": "/teste-jogo-da-velha-pwa/index.html"
+  }
+];
